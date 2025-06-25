e1b61d855c116b196f06a999e725cd3e290d8a3b
jP <silva.joao@icen.ufpa.br>
2025-06-24 21:44:11 -0300

    algoritimo finalizado

--- Teste.java
+++ Teste.java
@@ -1,11 +1,21 @@
 public class Teste{
     public static void main(String[] args){
-    int[] conjuntoA = {11, 13, 45, 7}, conjuntoB = {24, 4, 16, 81, 10, 12}, conjuntoR;
-    Uniao teste = new Uniao();
-    conjuntoR = new int[10];
-    System.out.println(conjuntoA);
-    System.out.println(conjuntoB);
-    teste.Unir(conjuntoA, conjuntoB, conjuntoR);
-    System.out.println(conjuntoR);
+    int[] conjuntoA = {11, 4, 16, 7}, conjuntoB = {17, 4, 16, 11, 10, 7}, conjuntoR;
+    int repetidos = Uniao.repetidos(conjuntoA, conjuntoB);
+    conjuntoR = new int[conjuntoA.length+conjuntoB.length-repetidos];
+
+
+    imprimir(conjuntoA);
+    imprimir(conjuntoB);
+
+    Uniao.Unir(conjuntoA, conjuntoB, conjuntoR);
+
+    imprimir(conjuntoR);
+    }
+    public static void imprimir(int[] lista){
+        for (int i =0; i<lista.length;i++){
+            System.out.print(lista[i]+" ");
+        }
+        System.out.println("");
     }
 }
--- Uniao.java
+++ Uniao.java
@@ -1,13 +1,38 @@
 public class Uniao {
-    public void Unir(int[] conjuntoA, int[] conjuntoB, int[] conjuntoR){
+    public static void Unir(int[] conjuntoA, int[] conjuntoB, int[] conjuntoR){
         int indice = 0;
-        for (int i = 0; i<conjuntoA.length-1;i++){
+        for (int i = 0; i<=conjuntoA.length-1;i++){
             conjuntoR[indice]=conjuntoA[i];
+            // System.out.println("item "+indice);
             indice++;
         }
-        for (int i = 0; i<conjuntoB.length-1;i++){
-            conjuntoR[indice]=conjuntoA[i];
-            indice++;
+        for (int itemB : conjuntoB){
+            boolean repetido = false;
+            for (int item : conjuntoA){
+                if (item == itemB){
+                    repetido = true;
+                }
+            }
+            if (repetido == false){
+                conjuntoR[indice]=itemB;
+                // System.out.println("item "+indice);
+                indice++;
+            }
         }
     }
+
+    public static int repetidos(int[] conjuntoA,int[] conjuntoB){
+        int contador = 0 ;
+        for (int itemA : conjuntoA){
+            for (int itemB : conjuntoB){
+                if (itemA == itemB){
+                    contador++;
+                }
+            }
+        }
+        // System.out.println("contador: "+contador);
+        return contador;
+    }
+
+
 }
